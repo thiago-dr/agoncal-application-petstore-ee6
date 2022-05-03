d6cc1cbab86aacbdae87db0565588825de9005b1
Thiago-DR <thiago.dr.1055@gmail.com>
2022-05-03 20:34:25 -0300

    Rafatoração usando Lombok (ToString)
    Substituindo código do metodo toString pela anotação Lombok

--- src/main/java/org/agoncal/application/petstore/domain/CartItem.java
+++ src/main/java/org/agoncal/application/petstore/domain/CartItem.java
@@ -2,6 +2,7 @@ package org.agoncal.application.petstore.domain;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import lombok.ToString;
 
 /**
  * @author Antonio Goncalves
@@ -9,6 +10,7 @@ import javax.validation.constraints.NotNull;
  *         --
  */
 
+@ToString
 public class CartItem {
 
     // ======================================
@@ -81,15 +83,5 @@ public class CartItem {
         int result = item.hashCode();
         result = 31 * result + quantity.hashCode();
         return result;
-    }
-
-    @Override
-    public String toString() {
-        final StringBuilder sb = new StringBuilder();
-        sb.append("CartItem");
-        sb.append("{item='").append(item).append('\'');
-        sb.append(", quantity='").append(quantity).append('\'');
-        sb.append('}');
-        return sb.toString();
     }
 }
