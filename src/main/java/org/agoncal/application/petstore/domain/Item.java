46e1f656eff70efa9eaeb0aa479fe5cbb631ff42
Thiago-DR <thiago.dr.1055@gmail.com>
2022-05-03 19:20:17 -0300

    Refatoração usando Lombok (Getter e Setter)
    Substituindo código dos getters e setters pela anotação Lombok

--- src/main/java/org/agoncal/application/petstore/domain/Item.java
+++ src/main/java/org/agoncal/application/petstore/domain/Item.java
@@ -8,7 +8,8 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
-
+import lombok.Getter;
+import lombok.Setter;
 /**
  * @author Antonio Goncalves
  *         http://www.antoniogoncalves.org
@@ -22,6 +23,8 @@ import javax.xml.bind.annotation.XmlTransient;
         @NamedQuery(name = Item.FIND_ALL, query = "SELECT i FROM Item i")
 })
 @XmlRootElement
+@Getter
+@Setter
 public class Item {
 
     // ======================================
@@ -67,54 +70,6 @@ public class Item {
         this.unitCost = unitCost;
         this.imagePath = imagePath;
         this.product = product;
-        this.description = description;
-    }
-
-    // ======================================
-    // =         Getters & setters          =
-    // ======================================
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Float getUnitCost() {
-        return unitCost;
-    }
-
-    public void setUnitCost(Float unitCost) {
-        this.unitCost = unitCost;
-    }
-
-    public String getImagePath() {
-        return imagePath;
-    }
-
-    public void setImagePath(String imagePath) {
-        this.imagePath = imagePath;
-    }
-
-    public Product getProduct() {
-        return product;
-    }
-
-    public void setProduct(Product product) {
-        this.product = product;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
         this.description = description;
     }
 
